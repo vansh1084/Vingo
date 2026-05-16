09f50f60b355e6523371a95635564dfaec96f6d2
vansh1084 <shivpatel1084@gmail.com>
2026-05-17 01:33:58 +0530

    fixed dashboard filename

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import UserDashboard from '../components/UserDashboard'
+import UserDashboard from '../components/temp'
 import OwnerDashboard from '../components/OwnerDashboard'
 import DeliveryBoy from '../components/DeliveryBoy'
 
